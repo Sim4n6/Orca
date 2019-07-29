6abbd2d922e0867f6d5edea42176bb959cb29415
Sim4n6 <Sim4n6@gmail.com>
2019-07-29 14:48:54 +0100

    finished the cmd line arguments structure

--- bh2db.py
+++ bh2db.py
@@ -1,11 +1,18 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="Process .bash_history via sqlite3 db")
-parser.add_argument("--import", '-i', help="import a db file", type=str, metavar='db')
-parser.add_argument("--search", "-s", help="search for the term", type=str, metavar='term')
-parser.add_argument("--count",'-c', help="count items in history")
-parser.add_argument("--version","-v", help="print current script version")
+VERSION = "0.1"
+
+parser = argparse.ArgumentParser(prog='bh2db', description="Process .bash_history file via sqlite3 database.")
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-p', "--process", help="process a .bash_history file into an sqlite db.", type=str, metavar='bash_history')
+group.add_argument('-i', "--import", help="import a sqlite db file.", type=str, metavar='file.db')
+parser.add_argument('-c', "--count", help="count the number of items in history.", action='store_const', const=-1)
+parser.add_argument('-s', "--search",  help="search for the term in the db.", type=str, metavar='term')
+parser.add_argument('-v', "--version", help="print the current script version", action='version', version=f"Version {VERSION}")
 args = parser.parse_args()
 
 if args.search:
     print(args.search)
+
+if args.count:
+    print(args.count) # print -1 the value of const.
