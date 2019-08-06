4187713e73defce75361b24c53060c68b23b26e1
Sim4n6 <Sim4n6@gmail.com>
2019-08-06 14:41:50 +0100

    improvements

--- orca.py
+++ orca.py
@@ -1,10 +1,14 @@
 import argparse
 import sqlite3
 
+#some constants
 VERSION = "0.1"
+DB_NAME = "bash_history.db"
+
 
 # arguments parsing 
-parser = argparse.ArgumentParser(prog='Orca', description="Process .bash_history file via sqlite3 database.", epilog=""" orca.py --process Samples\.bash_history """)
+parser = argparse.ArgumentParser(prog='Orca', description="Process .bash_history file via sqlite3 database.", epilog=r""" orca.py --process Samples\.bash_history1 --count 
+    orca.py --load bash_history.db --lastcmd""")
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-p', "--process", help="process a .bash_history file into an sqlite db.", type=str, metavar='bash_history')
 group.add_argument('-lo', "--load", help="load a sqlite db file.", type=str, metavar='file.db')
@@ -17,19 +21,19 @@ parser.add_argument('-v', "--version", help="print the current script version", 
 args = parser.parse_args()
 
 if args.process:
-    print(args.process) # print a .bash_history
+    #print(args.process) # print a .bash_history
     
     # read lines of bash_history file
     f_bh = open(args.process)
     tokens_lst = []
     for line in f_bh :
         tokens = line.strip().split('  ')
-        print(tokens[0], tokens[1])
+        #print(tokens[0], tokens[1])
         tokens_lst.append(tokens)
     f_bh.close()
 
     # create a DB and insert all values in it
-    conn = sqlite3.connect("bash_history.db")
+    conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     c.execute(""" CREATE TABLE IF NOT EXISTS bash_history (ids int, commands text, UNIQUE(ids, commands))""")
     for tokens in tokens_lst:
@@ -38,28 +42,28 @@ if args.process:
     conn.close()
 
 if args.load:
-    print(args.load)
+    #print(args.load)
 
     conn = sqlite3.connect(args.load)
     c = conn.cursor()
-    conn.close()
+    #conn.close()
 
 if args.count:
-    print(args.count) # print -1 the value of const.
+    #print(args.count) # print -1 the value of const.
 
     # count the number of ids in table bash_history
-    conn = sqlite3.connect("bash_history.db")
+    conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     c.execute(""" SELECT COUNT(ids) FROM bash_history """)
     count = c.fetchone()[0]
     conn.close()
 
-    print(f"Number of items in bash_history.db : {count}")
+    print(f"Number of items in {DB_NAME} : {count}")
 
 if args.list:
-    print(args.list)
+    #print(args.list)
 
-    conn = sqlite3.connect("bash_history.db")
+    conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     c.execute(""" SELECT * FROM bash_history """)
     rows = c.fetchall()
@@ -68,9 +72,9 @@ if args.list:
     conn.close()
 
 if args.lastcmd:
-    print(args.lastcmd)
+    #print(args.lastcmd)
 
-    conn = sqlite3.connect("bash_history.db")
+    conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     c.execute(""" SELECT * FROM bash_history """)
     rows = c.fetchall()
@@ -80,9 +84,9 @@ if args.lastcmd:
 
 
 if args.search:
-    print(args.search)
+    #print(args.search)
 
-    conn = sqlite3.connect("bash_history.db")
+    conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     c.execute(""" SELECT * FROM bash_history WHERE commands = ?""",(args.search,))
     rows = c.fetchall()
@@ -94,7 +98,7 @@ if args.search:
     conn.close()
 
 if args.export:
-    print(args.export)
+    #print(args.export)
 
     if args.export == "csv":
         pass
